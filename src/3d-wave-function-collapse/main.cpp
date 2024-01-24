4e2f5aa4985b0c2b81bd051bfe1daa964adb07a5
Sebastian Dawid <sdawid@techfak.uni-bielefeld.de>
2024-01-24 10:03:30 +0100

    Started to implement gltf_tiles.

--- src/3d-wave-function-collapse/main.cpp
+++ src/3d-wave-function-collapse/main.cpp
@@ -6,6 +6,69 @@
 
 #include <glm/gtx/transform.hpp>
 
+#include <format>
+
+struct gltf_tile_t : wfc::tile_i
+{
+    // NORTH, EAST, SOUTH, WEST, UP, DOWN
+    std::array<bool, 6> faces = {};
+    float weight = 1.f;
+    std::string gltf_path = "<none>";
+    glm::mat4 rotation = glm::mat4(1.f);
+
+    bool fits_dir(const tile_i& tile, const wfc::dir_t dir) const override
+    {
+        gltf_tile_t& t = (gltf_tile_t&) tile;
+        switch (dir) {
+
+            case wfc::dir_t::NORTH:
+                return this->faces[0] == t.faces[2];
+            case wfc::dir_t::EAST:
+                return this->faces[1] == t.faces[3];
+            case wfc::dir_t::SOUTH:
+                return this->faces[2] == t.faces[0];
+            case wfc::dir_t::WEST:
+                return this->faces[3] == t.faces[1];
+            case wfc::dir_t::UP:
+                return this->faces[4] == t.faces[5];
+            case wfc::dir_t::DOWN:
+                return this->faces[5] == t.faces[4];
+            default:
+                return false;
+        }
+    };
+
+    float get_weight() const override
+    {
+        return this->weight;
+    };
+
+    bool operator==(const tile_i& other) const override
+    {
+        return false;
+    };
+ 
+    bool operator==(const gltf_tile_t& other) const
+    {
+        return this->rotation == other.rotation && this->gltf_path == other.gltf_path;
+    };
+
+    gltf_tile_t(std::string path, glm::mat4 rotation, std::array<bool, 6> faces, float weight = 1.f)
+    {
+        this->gltf_path = path;
+        this->rotation = rotation;
+        this->faces = faces;
+        this->weight = weight;
+    }
+};
+
+template<>
+std::vector<gltf_tile_t> wfc::superposition_t<gltf_tile_t>::all_possibilities = {
+    gltf_tile_t("", glm::mat4(1.f), { 0 }), // empty tile
+    gltf_tile_t("/assets/tileset/all.glb", glm::mat4(1.f), { true, true, true, true, true, true }), // all connections
+    gltf_tile_t("/assets/tileset/all_but_one.glb", glm::mat4(1.f), { true, true, true, true, false, true }) // all connections but up
+};
+
 int main()
 {
     std::string path = std::filesystem::current_path().string() + "/assets/tileset";
@@ -26,6 +89,7 @@ int main()
         return EXIT_FAILURE;
     }
 
+    /*
     std::size_t count = 0;
     for (const auto& entry : std::filesystem::directory_iterator(path))
     {
@@ -35,7 +99,19 @@ int main()
         }
         engine.loaded_scenes[std::format("{}", count)]->transform = glm::translate(glm::vec3(2 * count, 0, 0));
         count++;
-    }
+    }*/
+    wfc::superposition_t<gltf_tile_t> gltf_superposition;
+    gltf_superposition.possibilities = gltf_superposition.all_possibilities;
+
+    wfc::map_t<gltf_tile_t> gltf_map(4, 4);
+    for (int i = 0; i < 16; ++i)
+        gltf_map.map.push_back(gltf_superposition);
+    bool success = false;
+    do {
+        for (int i = 0; i < 16; ++i)
+            gltf_map.map[i] = gltf_superposition;
+        success = wfc::wave_function_collapse(gltf_map);
+    } while (!success);
 
     engine.run();
 
